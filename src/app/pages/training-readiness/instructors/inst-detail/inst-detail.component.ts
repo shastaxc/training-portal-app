1abccd052731fe19018dec6f0141cb1fac1213b3
Dakota Jordan <Dakota.Jordan@cesicorp.com>
2017-12-13 18:12:01 -0500

    Access to route parameter for inst-detail

--- src/app/pages/training-readiness/instructors/inst-detail/inst-detail.component.ts
+++ src/app/pages/training-readiness/instructors/inst-detail/inst-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from '../../../../../assets/docs/instructors.json';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-inst-detail',
@@ -10,10 +11,15 @@ export class InstDetailComponent implements OnInit {
 
   title = 'Instructor Detail';
   instructors = <any>data;
+  id: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+    this.route.params.subscribe(params => {
+      this.id = +params['id']; // (+) converts string 'id' to a number
 
+      // In a real app: dispatch action to load the details here.
+   });
+  }
 }
