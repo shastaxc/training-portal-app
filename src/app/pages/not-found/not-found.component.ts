46d85d333d1c3d414f6c4660c89196108ffbdc20
Dakota Jordan <Dakota.Jordan@cesicorp.com>
2017-12-14 10:51:50 -0500

    Added Functionality to Instructors Detail

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -10,7 +10,7 @@ export class NotFoundComponent implements OnInit {
 
   title = '404';
 
-  constructor(public _location: Location) { }
+  constructor(private _location: Location) { }
 
   goBack() {
     this._location.back();
--- src/app/pages/training-readiness/instructors/inst-detail/inst-detail.component.ts
+++ src/app/pages/training-readiness/instructors/inst-detail/inst-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from '../../../../../assets/docs/instructors.json';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-inst-detail',
@@ -12,14 +13,34 @@ export class InstDetailComponent implements OnInit {
   title = 'Instructor Detail';
   instructors = <any>data;
   id: number;
+  details: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private _location: Location) { }
+
+  goBack() {
+    this._location.back();
+  }
+
+  parseDateTime(datetime: string) {
+    // Assumes format DDMONYYYYHHMM(Z) -- DayMonthYearHourMin(Timezone)
+    // Timezone is +N or -N where N is number of hours diff from UTC
+    const day: string = datetime.substring(0, 2);
+    const month: string = datetime.substring(2, 5);
+    const year: string = datetime.substring(5, 9);
+    const time: string = datetime.substring(9, 13);
+    const timezone: string = datetime.substring(14, 16);
+    return day + ' ' + month + ' ' + year + ' ' + time + ' (UTC' + timezone + ')';
+  }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.id = +params['id']; // (+) converts string 'id' to a number
 
-      // In a real app: dispatch action to load the details here.
+      this.instructors.forEach(element => {
+        if (element.id === params['id']) {
+          this.details = element;
+        }
+      });
    });
   }
 }
