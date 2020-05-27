c3477bb12cfdecca4e6fb27f1dfa55fabf3682e9
shastaxc <shastaxc@gmail.com>
2020-05-27 00:50:19 -0400

    Auto-login for demo purposes.

--- src/app/components/login-menu/login-menu.component.ts
+++ src/app/components/login-menu/login-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-login-menu',
@@ -11,6 +12,7 @@ export class LoginMenuComponent implements OnInit {
   constructor(public auth: AuthService) { }
 
   ngOnInit() {
+    timer(1000).subscribe(() => this.auth.login());
   }
 
 }
